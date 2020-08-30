b15f3332aaca1ac83b29cebc18a6d6247424e905
shreyasrh9 <shreyas.rh9@gmail.com>
2020-08-30 14:06:02 +0530

    IN progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,7 +165,7 @@
 
   // Events carousel (uses the Owl Carousel library)
   $(".events-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     dots: true,
     loop: true,
     items: 1,
